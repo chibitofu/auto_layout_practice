ad5246ac74d29f901f7d61f1e464da0eec3c1f9e
chibitofu <erinmoony@gmail.com>
2017-10-12 15:03:33 -0700

    Initial Commit

---
+++ auto_layout_practiceTests/auto_layout_practiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  auto_layout_practiceTests.swift
+//  auto_layout_practiceTests
+//
+//  Created by Erin Moon on 10/12/17.
+//  Copyright © 2017 Erin Moon. All rights reserved.
+//
+
+import XCTest
+@testable import auto_layout_practice
+
+class auto_layout_practiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
